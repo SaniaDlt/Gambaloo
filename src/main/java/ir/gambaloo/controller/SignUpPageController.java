188e799f66d1e8eb6d6dad205f2a9a19e7995b52
SaniaDlt <sania.dolatabadi@mail.um.ac.ir>
2023-06-04 10:36:32 +0330

    signuppageadded

--- src/main/java/ir/gambaloo/controller/SignUpPageController.java
+++ src/main/java/ir/gambaloo/controller/SignUpPageController.java
@@ -2,38 +2,34 @@ package ir.gambaloo.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
+
+
 public class SignUpPageController {
 
     @FXML
-    private TextField addressTFLD;
+    private TextField addressFLD;
 
     @FXML
-    private TextField emailTFLD;
+    private TextField emailFLD;
 
     @FXML
-    private PasswordField passwordTFLD;
+    private PasswordField passwordFLD;
 
     @FXML
-    private HBox passwrodErrorLBL;
+    private TextField phonenumberFLD;
 
     @FXML
-    private TextField phonenumberTFLD;
+    private HBox usernameFLD;
 
     @FXML
-    private Button signUpBTN;
-
-    @FXML
-    private TextField usernameTFLD;
-
-    @FXML
-    void signUp(ActionEvent event) {
+    void signUPBTN(ActionEvent event) {
 
     }
 
 }
+
 
