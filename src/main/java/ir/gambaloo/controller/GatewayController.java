145e5faa1fe67bf3d847a647a67bd3c8b1b877e8
AliRje <121222311+AliRje82@users.noreply.github.com>
2023-06-12 13:45:15 +0430

    Gateway Added

---
+++ src/main/java/ir/gambaloo/controller/GatewayController.java
@@ -0,0 +1,29 @@
+package ir.gambaloo.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class GatewayController {
+
+    @FXML
+    private TextField captcha;
+
+    @FXML
+    private TextField cardNumberFLD;
+
+    @FXML
+    private Label code;
+
+    @FXML
+    private Label nameLbl;
+
+    @FXML
+    private PasswordField passwordFLD;
+
+    @FXML
+    private Label price;
+
+}
+
