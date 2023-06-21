ce41cca60357349d888ee7afad1be4e7c2e8e831
alirje82 <ali.rajaei@mail.um.ac.ir>
2023-06-21 11:36:57 +0330

    conflict fix

--- src/main/java/ir/gambaloo/controller/GatewayController.java
+++ src/main/java/ir/gambaloo/controller/GatewayController.java
@@ -1,12 +1,20 @@
 package ir.gambaloo.controller;
 
+import ir.gambaloo.Main;
+import ir.gambaloo.module.TaxGateway;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class GatewayController {
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class GatewayController implements Initializable {
 
     @FXML
     private TextField captcha;
@@ -42,9 +50,41 @@ public class GatewayController {
     @FXML
     private Label price;
     @FXML
+    private Label errorLbl;
+    private int captchaCode;
+    @FXML
     void payBTN( ActionEvent event) {
+        String cardnumber = cardNumberFLD.getText().replaceAll("[^0-9]", "");
+        if(captchaCode!=Integer.parseInt(captcha.getText())){
+            captchaMaker();
+        }else if(cardnumber.length()!=16){
+            errorLbl.setText("Wrong card");
+        } else if (passwordFLD.getText().length()!=4) {
+            errorLbl.setText("Wrong Password");
+        }else{
+            Node source= (Node) event.getSource();
+            source.getScene().getWindow().hide();
+            if(nameLbl.getText().toLowerCase().contains("tax")){
+                double money=Double.parseDouble(price.getText());
+                Main.client.addBalance(money/(1+ TaxGateway.taxRate));
+            }else{
+                Main.client.addBalance(Double.parseDouble((price.getText())));
+            }
+        }
 
     }
-    
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        captchaMaker();
+    }
+    public void captchaMaker(){
+        Random random=new Random();
+        captchaCode=random.nextInt(9000)+1000;
+        code.setText(""+captchaCode);
+    }
+
+
 }
 
